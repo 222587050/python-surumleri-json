25b479224ee4bfde0695b233520d2a7a19a05917
222587050 <259554289+222587050@users.noreply.github.com>
2026-02-25 18:09:03 +0300

    Create json_oku.py

---
+++ json_oku.py
@@ -0,0 +1,13 @@
+import json
+
+dosya_adi='devcontainer.json'
+
+with open(dosya_adi, 'r', encoding='utf-8') as dosya:
+  veriler = json.load(dosya)
+
+print("python surumeri listesi")
+print("-" * 25)
+
+for surum in veriler ["python_surumleri"]:
+  print(f"Versiyon : {surum['versiyon']} -> {surum['durum']}")
+  
